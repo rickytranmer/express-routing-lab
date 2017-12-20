ede1684657709e01e4ee8461ec4bffdb4127ebe3
rickytranmer <rwtranmer@gmail.com>
2017-12-20 16:55:03 -0700

    update comments

--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -7,22 +7,25 @@ var candies = [	{"id":1,"name":"Chewing Gum","color":"Red"},
 				{"id":3,"name":"Marshmallow","color":"Pink"},
 				{"id":4,"name":"Candy Stick","color":"Blue"} ];
 
+// - GET all candies
 router.get('/', function(req, res) {
 	res.send(candies);
 });
+// - GET specific candy
 router.get('/:index', function(req, res) {
 	res.send(candies[req.params.index-1]);
 });
+// - POST new candy
 router.post('/', function(req, res) {
 	candies.push(req.body);
 });
+// - PUT updated candy in given index
 router.put('/:index', function(req, res) {
 	candies[req.params.index-1] = req.body;
 });
+// - DELETE specific candy
 router.delete('/:index', function(req, res) {
 	candies[req.params.index-1] = null;
 });
-
-// Fill out the rest of the routes here
 
 module.exports = router;
