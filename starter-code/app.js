8b5bb0cdb2af7946564737d0382d069fce664d2b
rickytranmer <rwtranmer@gmail.com>
2017-12-20 16:52:56 -0700

    adds routes to candyRouter.js

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,17 +1,13 @@
-//Did you use npm install to
-//add all these packages
-//to our project?
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-
-// How do we 'require' the candyRouter file?
-var candyRouter;
+var candyRouter = require('./candyRouter');
 
 app.use(bodyParser.json());
 
-//How do we redirect the /candies path
-//through our candyRouter?
-//Hint: you need app.use
+app.use(express.static('public'));
+app.use('/candies', candyRouter);
 
-app.listen(3000);
+app.listen(3000, function() {
+	console.log('eat my shorts');
+});
--- starter-code/candyRouter.js
+++ starter-code/candyRouter.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-router = express.Router();
+var router = express.Router();
 
-var candies;
+var candies = [	{"id":1,"name":"Chewing Gum","color":"Red"},
+				{"id":2,"name":"Pez","color":"Green"},
+				{"id":3,"name":"Marshmallow","color":"Pink"},
+				{"id":4,"name":"Candy Stick","color":"Blue"} ];
 
-//What would need to go into candies
-//in order to pass our first test?
-
-router.get('/', function(req,res) {
-	// What would go here? 
-	// Hint: we want all candies in JSON format
+router.get('/', function(req, res) {
+	res.send(candies);
+});
+router.get('/:index', function(req, res) {
+	res.send(candies[req.params.index-1]);
+});
+router.post('/', function(req, res) {
+	candies.push(req.body);
+});
+router.put('/:index', function(req, res) {
+	candies[req.params.index-1] = req.body;
+});
+router.delete('/:index', function(req, res) {
+	candies[req.params.index-1] = null;
 });
 
 // Fill out the rest of the routes here
